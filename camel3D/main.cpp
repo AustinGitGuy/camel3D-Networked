4ba4301e6d729422148aadbb89f7606c4f7c2364
cedric.brownewell <cedric.brownewell@mymail.champlain.edu>
2020-01-26 00:12:06 -0500

    Implemented command architecture, display commands function, and most of private messaging + disconnection notification.

--- camel3D/main.cpp
+++ camel3D/main.cpp
@@ -11,7 +11,7 @@
 #include "Raknet/RakNetTypes.h"
 
 enum GameMessages{
-	ID_GAME_MESSAGE_1 = ID_USER_PACKET_ENUM + 1,ID_NAME
+	ID_GAME_MESSAGE_1 = ID_USER_PACKET_ENUM + 1, ID_NAME_JOIN, ID_NAME_LEAVE, CLIENT_LIST
 };
 
 const unsigned int MAXCLIENTS = 10;
@@ -40,12 +40,32 @@ void SendToClient(RakNet::RakPeerInterface* peer, ProfileList* clientProfiles, M
 	if(client == -1){
 		for(int i = 0; i < clientProfiles->iter; i++){
 			peer->Send((char*)&msg, sizeof(MsgStruct), HIGH_PRIORITY, RELIABLE_ORDERED, 0, clientProfiles->profiles[i].address, false);
+			
 		}
 	}
 	else {
 		peer->Send((char*)&msg, sizeof(MsgStruct), HIGH_PRIORITY, RELIABLE_ORDERED, 0, clientProfiles->profiles[client].address, false);
 	}
 }
+
+//Prints all connected cilents and their IP addresses
+void PrintClientList(RakNet::RakPeerInterface* peer, ProfileList* clientProfiles, MsgStruct msg)
+{
+	for (int i = 0; i < clientProfiles->iter; i++)
+	{
+		peer->Send((char*)& msg, sizeof(MsgStruct), HIGH_PRIORITY, RELIABLE_ORDERED, 0, clientProfiles->profiles[-1].address, false);
+	}
+
+
+}
+
+//Below Fucntion is used to display all available commands for program
+void DisplayCommands() 
+{
+	printf("/help : Displays available commands\n /private : Send a private message to the defined user (/private Username Message)\n /cilents Displays List of currently connected clients (Host Only) /quit : Return to chatroom selection.%s\n");
+}
+
+
 
 void PacketHandler(RakNet::RakPeerInterface* peer, bool isServer, unsigned int maxClients, unsigned int serverPort, UserProfile* profile, ProfileList* clientProfiles){
 	char msg[127];
@@ -59,6 +79,17 @@ void PacketHandler(RakNet::RakPeerInterface* peer, bool isServer, unsigned int m
 			switch(packet->data[0]){
 			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
 				printf("Another client has disconnected.\n");
+				//When a client has disconnected display message saying they have left
+				MsgStruct send;
+				send.id = (RakNet::MessageID)ID_NAME_LEAVE;
+				strcpy(send.msg, clientProfiles->profiles[0].name);
+
+				//Cast to a char* to send the struct as a packet
+				peer->Send((char*)& send, sizeof(MsgStruct), HIGH_PRIORITY, RELIABLE_ORDERED, 0, packet->systemAddress, false);
+
+				//Remove Disconnected user from profile list
+				//profile->address = packet->systemAddress;
+				//profile->isHost = true;
 				break;
 			case ID_REMOTE_CONNECTION_LOST:
 				printf("Another client has lost the connection.\n");
@@ -71,7 +102,7 @@ void PacketHandler(RakNet::RakPeerInterface* peer, bool isServer, unsigned int m
 
 				// Use a struct to send the id of the game
 				MsgStruct send;
-				send.id = (RakNet::MessageID)ID_NAME;
+				send.id = (RakNet::MessageID)ID_NAME_JOIN;
 				strcpy(send.msg, clientProfiles->profiles[0].name);
 
 				//Cast to a char* to send the struct as a packet
@@ -90,6 +121,7 @@ void PacketHandler(RakNet::RakPeerInterface* peer, bool isServer, unsigned int m
 			case ID_DISCONNECTION_NOTIFICATION:
 				if(isServer){
 					printf("A client has disconnected.\n");
+
 				}
 				else {
 					printf("We have been disconnected.\n");
@@ -116,7 +148,7 @@ void PacketHandler(RakNet::RakPeerInterface* peer, bool isServer, unsigned int m
 				break;
 			}
 
-			case ID_NAME: {
+			case ID_NAME_JOIN: {
 
 				//If we are getting a name then store it for later.
 				//Since this is the server we can still use the profileList struct
@@ -128,6 +160,30 @@ void PacketHandler(RakNet::RakPeerInterface* peer, bool isServer, unsigned int m
 				clientProfiles->iter++;
 				break;
 			}
+			case ID_NAME_LEAVE: {
+
+				//If we are getting a name then store it for later.
+				//Since this is the server we can still use the profileList struct
+				MsgStruct* read = (MsgStruct*)packet->data;
+				strcpy(clientProfiles->profiles[clientProfiles->iter].name, read->msg);
+				clientProfiles->profiles[clientProfiles->iter].address = packet->systemAddress;
+
+				printf(clientProfiles->profiles[clientProfiles->iter].name, "has disconnected %s\n");
+				clientProfiles->iter--;
+				break;
+			}
+
+			case CLIENT_LIST: {
+
+				MsgStruct* read = (MsgStruct*)packet->data;
+				strcpy(clientProfiles->profiles[clientProfiles->iter].name, read->msg);
+				clientProfiles->profiles[clientProfiles->iter].address = packet->systemAddress;
+
+				printf("Client connected with name %s\n", clientProfiles->profiles[clientProfiles->iter].name);
+				clientProfiles->iter++;
+				break;
+			}
+
 			default:
 				printf("Message with identifier %i has arrived.\n", packet->data[0]);
 				break;
@@ -145,13 +201,19 @@ int main(void){
 	UserProfile profile; //This is for the server (if we are client)
 	ProfileList clientProfiles;
 	bool isServer;
+	bool running = true;
+	bool programTrue;//Used to reset back to lobby
 
-	printf("C to connect to a chat room or H to host a chat room\n");
+
+	printf("C to connect to a chat room or H to host a chat room or Q to quit the program\n");
 	fgets(str, 127, stdin);
 	if ((str[0] == 'c') || (str[0] == 'C')){
 		RakNet::SocketDescriptor sd;
 		peer->Startup(1, &sd, 1);
 		isServer = false;
+	}
+	if ((str[0] == 'q') || (str[0] == 'Q')) {
+		running = false;
 	}
 	//You can actually type anything other than c for hosting
 	else {
@@ -187,7 +249,7 @@ int main(void){
 		peer->Connect(str, serverPort, 0, 0);
 	}
 
-	bool running = true;
+	
 
 	//Start a new thread to handle packets so we can use this one to run input manager
 	std::thread handlePackets(PacketHandler, peer, isServer, MAXCLIENTS, serverPort, &profile, &clientProfiles);
@@ -205,12 +267,13 @@ int main(void){
 				char tmp[127];
 
 				if(isServer){
-					strcpy(tmp, "Server: ");
+					strcpy(tmp, "Server: ");//Appends Server to Beginning of Message
 				}
 				else {
-					strcpy(tmp, clientProfiles.profiles[0].name);
+					strcpy(tmp, clientProfiles.profiles[0].name);//Appends Cilent Username to Beginning of Message
 				}
-				
+
+				strcpy(tmp, "(Public) ");//Appends a notification that Message is public
 
 				MsgStruct send;
 				send.id = (RakNet::MessageID)ID_GAME_MESSAGE_1;
@@ -226,6 +289,71 @@ int main(void){
 			}
 			//Otherwise check command
 			else {
+
+				if (str[1] == 'h')//Displays list of commands
+				{
+					DisplayCommands();
+				}
+				else if(str[1] == 'p')//Private message
+				{
+					RakNet::SystemAddress dirAddress;
+
+					for (int i = 0; i < clientProfiles.iter; i++)
+					{
+						if (clientProfiles.profiles[i].name == )
+						{
+							dirAddress = clientProfiles.profiles[i].address;
+							break;
+						}
+						else
+						{
+
+						}
+					}
+
+					char* nameEnd;
+					nameEnd = strchr(str, '\n');
+					*nameEnd = ' ';
+
+					char tmp[127];
+
+					if (isServer) {
+						strcpy(tmp, "Server: ");//Appends Server to Beginning of Message
+					}
+					else {
+						strcpy(tmp, clientProfiles.profiles[0].name);//Appends Client Username to Beginning of Message
+					}
+
+					strcpy(tmp, "(Whisper) ");//Appends a notification that Message is private
+
+					MsgStruct send;
+					send.id = (RakNet::MessageID)ID_GAME_MESSAGE_1;
+					strcpy(send.msg, strcat(tmp, str));
+
+					peer->Send((char*)& send, sizeof(MsgStruct), HIGH_PRIORITY, RELIABLE_ORDERED, 0, dirAddress, false);
+
+				}
+				else if (str[1] == 'c')//Displays list of connected clients
+				{
+					if (isServer)//If server run command
+					{
+						//PrintClientList(peer, clientProfiles, )
+					}
+					else//If not server deny access
+					{
+						printf("Access to command denied.%s\n");
+					}
+				}
+				else if (str[1] == 'q')//Leaves current chat room and brings user back to the Lobby
+				{
+
+				}
+				else//If no command is recognized display message and display list of commands
+				{
+					printf("Invalid Command.  Please use one of the below commands.%s\n");
+					DisplayCommands();
+				}
+
 
 			}
 		}
